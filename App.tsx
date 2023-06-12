50fd7bd80f4b2e64be2d9ab800b219d941b57f18
luismda <luismigueldutraalves@gmail.com>
2023-06-12 14:23:17 -0300

    feat: create theme and add fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,31 @@
+import { StatusBar } from 'react-native'
+import { 
+  useFonts, 
+  Roboto_400Regular, 
+  Roboto_700Bold 
+} from '@expo-google-fonts/roboto'
+import { ThemeProvider } from 'styled-components'
+
+import { defaultTheme } from '@theme/default'
+
 import { Groups } from '@screens/Groups'
+import { Loading } from '@components/Loading'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
   return (
-    <Groups />
+    <ThemeProvider theme={defaultTheme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {hasLoadedFonts ? <Groups /> : <Loading />}
+    </ThemeProvider>
   )
 }
