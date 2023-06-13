d3972c4caeb2b79c35749335046813504d8237fb
luismda <luismigueldutraalves@gmail.com>
2023-06-13 09:19:47 -0300

    feat: create groups list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,31 @@
-import { GroupCard } from '@components/GroupCard'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+
 import { Container } from './styles'
 
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { GroupCard } from '@components/GroupCard'
+import { EmptyList } from '@components/EmptyList'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Turma do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <EmptyList message="Que tal criar a sua primeira turma?" />
+        )}
+      />
     </Container>
   )
 }
